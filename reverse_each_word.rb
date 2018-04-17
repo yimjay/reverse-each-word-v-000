c66be8d5c53774204f5533f5be289392fd61f818
Ji Yim <gsooyim@gmail.com>
2018-04-17 20:17:24 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+# Reverses given string without reversing order of words in string
+# Using each
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = []
+  array.each do |word|
+    new_array << word.reverse!
+  end
+  new_array.join(" ")
+end
+
+# Using collect
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.collect do |word|
+    word.reverse!
+  end
+  array.join(" ")
+end
