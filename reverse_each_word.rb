bf7f0d8fb2c479ad969422b1b20a5d884e23d05f
Ji Yim <gsooyim@gmail.com>
2018-04-17 20:21:21 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 # Reverses given string without reversing order of words in string
 # Using each
 def reverse_each_word(string)
-  array = string.split(" ")
+  array = string.split
   new_array = []
   array.each do |word|
     new_array << word.reverse!
@@ -9,11 +9,7 @@ def reverse_each_word(string)
   new_array.join(" ")
 end
 
-# Using collect
-def reverse_each_word(string)
-  array = string.split(" ")
-  array.collect do |word|
-    word.reverse!
-  end
-  array.join(" ")
-end
+# # Using collect
+# def reverse_each_word(string)
+#   string.split.collect { |word| word.reverse!}.join(" ")
+# end
